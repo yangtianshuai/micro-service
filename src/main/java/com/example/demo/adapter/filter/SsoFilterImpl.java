2bb0e61ffcc4c6e5146fa50401238c271c5a47ed
杨天帅 <myword@126.com>
2023-12-25 15:55:25 +0800

    增加示例说明

--- src/main/java/com/example/demo/adapter/filter/SsoFilterImpl.java
+++ src/main/java/com/example/demo/adapter/filter/SsoFilterImpl.java
@@ -3,11 +3,6 @@ package com.example.demo.adapter.filter;
 import api.config.sso.ISsoHandler;
 import api.config.sso.SsoCookie;
 import com.honlivhp.api.config.SsoFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpServletResponseWrapper;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 public class SsoFilterImpl extends SsoFilter {
 
@@ -18,11 +13,15 @@ public class SsoFilterImpl extends SsoFilter {
 
     @Override
     public void ValidateComplate(SsoCookie ssoCookie) {
-
+        //SSO登入
+        //令牌：ssoCookie.ID
+        //如果你不希望频繁进行验证，就需要在这里保存下来令牌
+        //保存令牌方式：Session、JWT
     }
 
     @Override
     public void LogoutComplate(SsoCookie ssoCookie) {
-
+        //SSO登出
+        //需要清除服务器中的Session（如果有）
     }
 }
