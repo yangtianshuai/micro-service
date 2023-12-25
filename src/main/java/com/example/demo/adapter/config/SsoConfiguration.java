855ee7c3a16a38851f4a82465326d3029f535195
杨天帅 <myword@126.com>
2023-12-25 15:47:52 +0800

    增加SSO配置

---
+++ src/main/java/com/example/demo/adapter/config/SsoConfiguration.java
@@ -0,0 +1,66 @@
+package com.example.demo.adapter.config;
+
+import api.config.sso.IPMapping;
+import api.config.sso.ISsoHandler;
+import api.config.sso.SsoOptions;
+import api.config.sso.cas.CasHandler;
+import api.config.sso.oauth2.OAuth2Handler;
+import api.config.sso.oauth2.OAuth2Mode;
+import api.config.sso.oauth2.OAuth2Options;
+import com.example.demo.adapter.filter.SsoFilterImpl;
+import com.honlivhp.api.config.SsoFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+
+@Configuration(proxyBeanMethods = false)
+public class SsoConfiguration {
+
+    /**
+     * CAS方式接入
+     * @return
+     */
+    @Bean
+    public SsoFilter useCas() {
+        SsoOptions options = new SsoOptions();
+
+        //建议从配置中加载
+        options.AppID = "应用ID";
+
+        options.BaseURL = "SSO服务器地址";
+
+        options.IPMappings = new ArrayList<>();
+        IPMapping maping = new IPMapping();
+
+        maping.server_ip="当前应用IP";
+        maping.base_url="http://SSO服务器IP:端口";//返回给前端浏览器，用于跳转
+        maping.validate_url="http://SSO服务器IP:端口";//当前应用获取校验令牌和获取用户信息，ps:用于应用无法通过maping.base_url地址访问SSO服务器的场景
+        options.IPMappings.add(maping);
+
+        ISsoHandler ssoHandler = new CasHandler(options);
+        return new SsoFilterImpl(ssoHandler);
+    }
+
+    /**
+     * OAuth2方式接入
+     * @return
+     */
+    @Bean
+    public SsoFilter useOAuth2() {
+        SsoOptions options = new SsoOptions();
+
+        //建议从配置中加载
+        options.AppID = "应用ID";
+        options.Secret = "应用密钥";
+        options.BaseURL = "SSO服务器地址";
+        options.IPMappings = new ArrayList<>();
+
+        OAuth2Options oAuth2Options = new OAuth2Options();
+        oAuth2Options.RedictUri = "回调地址";
+        oAuth2Options.Mode = OAuth2Mode.Simple;//使用简单模式，默认使用标准模式
+
+        ISsoHandler ssoHandler = new OAuth2Handler(options,oAuth2Options);
+        return new SsoFilterImpl(ssoHandler);
+    }
+}
